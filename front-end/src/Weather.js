447083e2654bf8a2c32472f4163a6876caaa79a0
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-05-08 14:27:46 -0700

    :white_check_mark: Front end

--- front-end/src/Weather.js
+++ front-end/src/Weather.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 class Weather extends React.Component {
   constructor(props) {
@@ -9,12 +10,31 @@ class Weather extends React.Component {
     };
   }
 
+  clickBtn = async () => {
+    try {
+      const url = `${process.env.REACT_APP_SERVER}/weatherData`;
+      const response = await axios.get(url);
+      this.setState({ weatherData: response.data }, () =>
+        console.log(this.state.weatherData)
+      );
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
   clickItem = () => console.log('clicked');
   render() {
     return (
       <>
         <p>Weather data</p>
-        <Button onClick={this.clickItem}>Click!</Button>
+        <Button onClick={this.clickBtn}>Click!</Button>
+
+        {this.state.weatherData.length > 0 &&
+          this.state.weatherData.map((item) => (
+            <>
+              <p>{item}</p>
+            </>
+          ))}
       </>
     );
   }

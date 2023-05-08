4a0d2ff36088a303e5ae987d7ee709a968b71563
Julio Ramirez <113139401+rzvdev1@users.noreply.github.com>
2023-05-08 00:18:12 -0700

    :construction: Testing server request

--- server.js
+++ server.js
@@ -17,8 +17,43 @@ app.get('/', (request, response) => {
   response.status(200).send('Route up and running');
 });
 
-app.get('/weatherData', (request, response) => {
-  response.status(200).send(weatherData);
+//render-app-name/weatherData
+app.get('/weatherData', (request, response, next) => {
+  const { dataList } = request.query;
+  console.log(dataList);
+
+  try {
+    // const description = weatherData[0].data[0].weather.description;
+    // const date = weatherData[0].data[0].valid_date;
+
+    if (dataList === 'Broken clouds') {
+      const formattedData = weatherData.data[0].map((obj) => new Forecast(obj));
+      console.log(formattedData);
+      response.status(200).send(formattedData);
+    } else if (dataList === 'Light rain') {
+      const formattedData = weatherData.data[1].map((obj) => new Forecast(obj));
+      console.log(formattedData);
+    } else {
+      response.status(404).send('Error no data');
+    }
+  } catch (error) {
+    next(error);
+  }
+});
+
+class Forecast {
+  constructor(Obj) {
+    this.date = Obj.valid_date;
+    this.description = Obj.weather.description;
+  }
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not Found');
+});
+
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
 });
 
 app.listen(PORT, () => console.log(` listening on ${PORT}`));
